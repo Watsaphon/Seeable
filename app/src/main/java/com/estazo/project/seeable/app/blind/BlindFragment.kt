ff7665e7c4f6d33662a8e8b525e3f00435b071c9
Estazo <estazo_wsp@hotmail.com>
2021-04-06 23:08:55 +0700

    //fixed MyFirebaseMessagingService to builder notification

--- app/src/main/java/com/estazo/project/seeable/app/blind/BlindFragment.kt
+++ app/src/main/java/com/estazo/project/seeable/app/blind/BlindFragment.kt
@@ -17,19 +17,18 @@ import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.estazo.project.seeable.app.R
 import com.estazo.project.seeable.app.databinding.FragmentBlindBinding
@@ -40,6 +39,8 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 import kotlinx.android.synthetic.main.alert_dialog_set_name.view.*
 import java.util.*
 
@@ -140,16 +141,32 @@ class BlindFragment : Fragment() {
         binding.callEmergency.setOnVeryLongClickListener{
             vibrate()
             textToSpeech!!.speak("Call Emergency Activate", TextToSpeech.QUEUE_FLUSH, null)
-            emergencyCall()
+//            emergencyCall()
             Toast.makeText(activity, getString(R.string.button_emergency_call), Toast.LENGTH_SHORT).show()
+            Firebase.messaging.subscribeToTopic("call_emergency").addOnCompleteListener { task ->
+                var msg = "Subscribed to call emergency topic"
+            if (!task.isSuccessful) {
+                msg = "Failed to subscribe to call Emergency topic"
+            }
+            Log.d("testNotification", msg)
+            Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+        }
         }
         binding.sendLocation.setOnVeryLongClickListener{
             vibrate()
             Log.d("Debug:","sharedLocationBtn -> CheckPermission : "  + checkPermission().toString())
             Log.d("Debug:", "sharedLocationBtn -> isLocationEnabled : " +  isLocationEnabled().toString())
             textToSpeech!!.speak("send Location Activate", TextToSpeech.QUEUE_FLUSH, null)
-            getLastLocation()
-            sendLocation()
+//            getLastLocation()
+//            sendLocation()
+            Firebase.messaging.subscribeToTopic("shared_location").addOnCompleteListener { task ->
+                var msg = getString(R.string.msg_subscribed)
+                if (!task.isSuccessful) {
+                     msg = getString(R.string.msg_subscribe_failed)
+                 }
+                Log.d("testNotification", msg)
+                Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+        }
         }
     }
 
