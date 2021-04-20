2e0b589396acc3d317d7e1f61ee95e04e04934f0
Puttipong-DonutBkk <puttipongs@donuts-bkk.com>
2021-04-20 15:32:51 +0700

    Add Tensorflow lite file and object detection for using in the app, implement camera using com.otaliastudios:cameraview https://github.com/natario1/CameraView

---
+++ app/src/main/java/com/estazo/project/seeable/app/objectDetection/TFObjectDetect.kt
@@ -0,0 +1,67 @@
+package com.estazo.project.seeable.app.objectDetection
+
+import android.content.Context
+import android.graphics.*
+import android.util.Log
+import com.estazo.project.seeable.app.ml.ModelMlV2
+import com.otaliastudios.cameraview.frame.Frame
+import org.tensorflow.lite.support.image.TensorImage
+import java.io.ByteArrayOutputStream
+
+class TFObjectDetect(context: Context) {
+    val model = ModelMlV2.newInstance(context)
+    var detect_num = ""
+    fun detect(frame: Frame) {
+
+        val out = ByteArrayOutputStream()
+        val yuv = YuvImage(
+            frame.getData(),
+            ImageFormat.NV21,
+            frame.size.width,
+            frame.size.height,
+            null
+        )
+        yuv.compressToJpeg(Rect(0, 0, frame.size.width, frame.size.height), 90, out);
+        val imageBytes: ByteArray = out.toByteArray()
+        val image = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+        val resized = Bitmap.createScaledBitmap(image, 512, 512, true);
+        // Creates inputs for reference.
+        val normalizedInputImageTensor = TensorImage.fromBitmap(resized)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(normalizedInputImageTensor)
+        val locations = outputs.locationsAsTensorBuffer
+        val classes = outputs.classesAsTensorBuffer
+        val scores = outputs.scoresAsTensorBuffer
+        val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer
+        val maxScore = scores.floatArray.max()
+        val maxPosition = maxScore?.let { scores.floatArray.indexOf(it) }
+        if (maxScore != null) {
+            if (maxScore > 0.9) {
+                maxPosition?.let { classes.floatArray[it].toString() }?.let {
+                    Log.d("Detect class: ", it)
+                    detect_num = maxScore.toString()
+                }
+/*
+                ส่งค่าไปที่ใช้ในการเด้ง Dialog
+                0 = ทางม้าลาย, 1 = ป้ายรถเมล์
+                detect_num.toInt()
+
+                ในหน้าที่ทำ navigate เพิ่มอันนี้ดวย
+                lateinit var camera: CameraView
+
+                * ใน onCreate() *
+                camera = findViewById(R.id.camera)
+                camera.setLifecycleOwner(this)
+                camera.addFrameProcessor {
+                    TFLiteDetection(this@*ActivityName*).detect(it)
+                }
+
+                แล้วเพิ่ม cameraView ใน layout ตั้งขนาดมันให้เป็น 1*1 dp พอ
+*/
+            }
+        }
+
+        model.close()
+    }
+}
